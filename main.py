df2a2e40b41c286d1e4731498eae4061efad2871
simakovpa <toli4@outlook.com>
2019-01-07 23:32:13 +0700

    Lesson 2 (child window) commit

--- main.py
+++ main.py
@@ -4,6 +4,32 @@ import tkinter as tk
 class Main(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
+        self.init_main()
+
+    def init_main(self):
+        toolbar = tk.Frame(bg='#d7d8e0', bd=2)
+        toolbar.pack(side=tk.TOP, fill=tk.X)
+
+        self.add_image = tk.PhotoImage(file='images/for-sale_48.png')
+        btn_open_dialog = tk.Button(toolbar, text='Добавить', command=self.open_dialog, bg='#d7d8e0', bd=0, compound=tk.TOP, image=self.add_image)
+        btn_open_dialog.pack(side=tk.LEFT)
+    
+    def open_dialog(self):
+        Child()
+
+
+class Child(tk.Toplevel):
+    def __init__(self):
+        super().__init__(root)
+        self.init_child()
+
+    def init_child(self):
+        self.title('Добавить доходы\расходы')
+        self.geometry('400x220+400+300')
+        self.resizable(False, False)
+
+        self.grab_set()
+        self.focus_set()
 
 
 if __name__ == "__main__":
