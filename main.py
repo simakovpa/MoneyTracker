9b7f96fceedbe7128aa0c67f76fdabcddb971fe5
simakovpa <toli4@outlook.com>
2019-01-07 23:59:47 +0700

    Lesson 3 - Table & Button Ok/Cancel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 import tkinter as tk
+from tkinter import ttk
 
 class Main(tk.Frame):
     def __init__(self, root):
@@ -13,6 +14,20 @@ class Main(tk.Frame):
         self.add_image = tk.PhotoImage(file='images/for-sale_48.png')
         btn_open_dialog = tk.Button(toolbar, text='Добавить', command=self.open_dialog, bg='#d7d8e0', bd=0, compound=tk.TOP, image=self.add_image)
         btn_open_dialog.pack(side=tk.LEFT)
+
+        self.tree = ttk.Treeview(self, columns=('ID', 'description', 'costs', 'total'), height=15, show='headings')
+
+        self.tree.column('ID', width=30, anchor=tk.CENTER)
+        self.tree.column('description', width=365, anchor=tk.CENTER)
+        self.tree.column('costs', width=150, anchor=tk.CENTER)
+        self.tree.column('total', width=100, anchor=tk.CENTER)
+
+        self.tree.heading('ID', text='ID')
+        self.tree.heading('description', text='Наименование')
+        self.tree.heading('costs', text='Статья дохода/расхода')
+        self.tree.heading('total', text='Сумма')
+
+        self.tree.pack()
     
     def open_dialog(self):
         Child()
@@ -27,6 +42,30 @@ class Child(tk.Toplevel):
         self.title('Добавить доходы\расходы')
         self.geometry('400x220+400+300')
         self.resizable(False, False)
+
+        label_description = tk.Label(self, text='Наименование')
+        label_description.place(x=50, y=50)
+        label_select = tk.Label(self, text='Статья дохода/расхода')
+        label_select.place(x=50, y=80)
+        label_sum = tk.Label(self, text='Сумма')
+        label_sum.place(x=50, y=110)
+
+        self.entry_description = ttk.Entry(self)
+        self.entry_description.place(x=200, y=50)
+
+        self.entry_money = ttk.Entry(self)
+        self.entry_money.place(x=200, y=110)
+
+        self.combobox = ttk.Combobox(self, values=[u'Доход', u'Расход'])
+        self.combobox.current(0)
+        self.combobox.place(x=200, y=80)
+
+        btn_cancel = ttk.Button(self, text='Закрыть', command=self.destroy)
+        btn_cancel.place(x=300, y=170)
+
+        btn_ok = ttk.Button(self, text='Добавить')
+        btn_ok.place(x=220, y=170)
+        btn_ok.bind('<Button-1>')
 
         self.grab_set()
         self.focus_set()
